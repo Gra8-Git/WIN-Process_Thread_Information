f36d8bc8d15bbbb27ff1a2a981d5e63cff318d8d
Gra8-Git <39712159+Gra8-Git@users.noreply.github.com>
2020-11-05 18:43:53 +0200

    Update Command line_PEB_PBI_x86_x64.cpp

--- Command line_PEB_PBI_x86_x64.cpp
+++ Command line_PEB_PBI_x86_x64.cpp
@@ -1,4 +1,3 @@
-/* compiled with mingw  and couple lib's :)   for x86 and x64 OS */
 #ifndef UNICODE
 #define UNICODE
 #define UNICODE_WAS_UNDEFINED
@@ -13,19 +12,11 @@
 #undef UNICODE
 #endif
 
-#ifdef _WOW64
-typedef UINT64 PTR;
-typedef ULONGLONG priv_ULONG;
-typedef PULONGLONG P_priv_ULONG;
-#define NT_QUERY_INFORMATION_PROCESS_NAME "NtWow64QueryInformationProcess64"
-#else
 typedef PVOID Privvalue;
 typedef ULONG priv_ULONG;
 typedef PULONG P_priv_ULONG;
 #define NT_QUERY_INFORMATION_PROCESS_NAME "NtQueryInformationProcess"
-#endif
 typedef int (*FN_NtQueryInformationProcess)(HANDLE, int, PVOID, priv_ULONG, P_priv_ULONG);
-
 #pragma commant(lib, "advapi32.lib");
 
 BOOL EnableWindowsPrivilege(BOOL State, HANDLE hprocess)
@@ -67,13 +58,13 @@ BOOL IsProcessUAC(HANDLE hprocess)
 
     if (!OpenProcessToken(hprocess, TOKEN_QUERY, &hToken))
     {
-        printf("\n Failed to get Process Token :%d.", GetLastError());
+        std::cout << "\n Failed to get Process Token : " << GetLastError() << "\n";
         goto Cleanup; // if Failed, we treat as False
     }
 
     if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
     {
-        printf("\nFailed to get Token Information :%d.", GetLastError());
+        std::cout << "\n Failed to get Token Information : " << GetLastError() << "\n";
         goto Cleanup; // if Failed, we treat as False
     }
 
@@ -101,14 +92,15 @@ _PROCESS_BASIC_INFORMATION64 readPibmemory(HANDLE hprocess)
     ntdll = LoadLibrary(L"ntdll.dll"); //load liberary and get hInstance to get function
     if (ntdll == NULL)
     {
-        puts("Unable to load ntdll");
+        std::cout << "Unable to load ntdll"
+                  << "\n";
         return pbi64;
     }
 
     NtQueryInformationProcess = (FN_NtQueryInformationProcess)GetProcAddress(ntdll, NT_QUERY_INFORMATION_PROCESS_NAME); //request function
     if (NtQueryInformationProcess == NULL)
     {
-        printf("Unable to get address of %s\n", NT_QUERY_INFORMATION_PROCESS_NAME);
+        std::cout << "Unable to get address of " << NT_QUERY_INFORMATION_PROCESS_NAME << "\n";
         return pbi64;
     }
 
@@ -117,7 +109,7 @@ _PROCESS_BASIC_INFORMATION64 readPibmemory(HANDLE hprocess)
     ret = NtQueryInformationProcess(hprocess, ProcessBasicInformation, &pbi64, sizeof(pbi64), &read_length); //get pbi  from the remote process
     if (ret != 0)
     {
-        printf("Error at %s, ret: %d\n", NT_QUERY_INFORMATION_PROCESS_NAME, ret);
+        std::cout << "Error at : " << NT_QUERY_INFORMATION_PROCESS_NAME << " ret : " << ret;
         return pbi64;
     }
 #else
@@ -125,7 +117,7 @@ _PROCESS_BASIC_INFORMATION64 readPibmemory(HANDLE hprocess)
     ret = NtQueryInformationProcess(hprocess, ProcessBasicInformation, &pbi, sizeof(pbi), &read_length); //get pbi  from the remote process
     if (ret != 0)
     {
-        printf("Error at %s, ret: %d\n", NT_QUERY_INFORMATION_PROCESS_NAME, ret);
+        std::cout << "Error at : " << NT_QUERY_INFORMATION_PROCESS_NAME << " ret : " << ret;
         return pbi64;
     }
 #endif
@@ -137,7 +129,7 @@ PEB readPEBmemory(HANDLE hprocess, _PROCESS_BASIC_INFORMATION64 pbi64)
     UINT64 dwSize = 0;
     if (!ReadProcessMemory(hprocess, pbi64.PebBaseAddress, &peb, sizeof(PEB), &dwSize))
     {
-        printf("Could not read the address of PEB!%d\n", GetLastError());
+        std::cout << "Could not read the address of PEB :" << GetLastError() << "\n";
     }
     return peb;
 }
@@ -149,7 +141,7 @@ PVOID readRTLUserProcessParam(HANDLE hprocess, PVOID peb)
     /* get the address of ProcessParameters */
     if (!ReadProcessMemory(hprocess, &(((_PEB *)peb)->ProcessParameters), &rtlUserProcParamsAddress, sizeof(PVOID), NULL))
     {
-        printf("Could not read the address of ProcessParameters!%d\n", GetLastError());
+        std::cout << "Could not read the address of ProcessParameters :" << GetLastError() << "\n";
     }
     return rtlUserProcParamsAddress;
 }
@@ -159,27 +151,23 @@ UNICODE_STRING readRTLcommand(HANDLE hprocess, PVOID rtlUserProcParamsAddress)
     UNICODE_STRING commandLine = {};
 
     SIZE_T *readBytes = new SIZE_T;
-    /* read the CommandLine UNICODE_STRING structure */
-
     /*We use PVOID to save address inside varible and create point to read memory  */
     if (!ReadProcessMemory(hprocess, &(((_RTL_USER_PROCESS_PARAMETERS *)rtlUserProcParamsAddress)->CommandLine), &commandLine, sizeof(commandLine), NULL))
     {
-        printf("Could not read CommandLine!  rtlUserProcParamsAddress.CommandLine %d\n", GetLastError());
+        std::cout << "Could not read ->  rtlUserProcParamsAddress.CommandLine :" << GetLastError() << "\n";
     }
     return commandLine;
 }
 
 WCHAR *readRTLcommandLine(HANDLE hprocess, UNICODE_STRING commandLine)
 {
-    const int MAX_SIZE = 512;
-    SIZE_T readBytes = 0;
     WCHAR *commandLineContents;
 
     commandLineContents = (WCHAR *)malloc(commandLine.Length);
     /* read the command line */
     if (!ReadProcessMemory(hprocess, commandLine.Buffer, commandLineContents, commandLine.Length, NULL))
     {
-        printf("Could not read the command line   commandLine.Buffer string! %p\n", GetLastError());
+        std::cout << "Could not read  commandLine.Buffer string" << GetLastError() << "\n";
     }
 
     return commandLineContents;
@@ -190,90 +178,56 @@ int main(void)
     HANDLE pToken = NULL;
     HANDLE hprocess;
     PVOID rtlUserProcParamsAddress;
+    PVOID BeingDebugged;
     UNICODE_STRING commandLine;
     WCHAR *commandLineContents;
 
     PEB peb;
     PVOID pebbaseaddress;
     //process id to enable external debug mode
-    int PID = 6316;
+    int PID = 14440;
     hprocess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, PID);
 
     if (IsProcessUAC(hprocess) == TRUE)
     {
-        printf("process Elevated");
+        std::cout << "\t\tProcess Elevated\n";
     }
     else
     {
-        printf("procen not elevated");
+        std::cout << "\t\tProcess not elevated\n";
     }
     if (!EnableWindowsPrivilege(TRUE, hprocess))
     {
-        printf("Could not enable Debug!\n");
+        std::cout << "\t\tNot  Debugged!\n\n";
         return 1;
     }
     else
     {
-        printf("SE_DEBUG_NAME: privilages enabled\n");
+        std::cout << "\t\tSE_DEBUG_NAME: privilages enabled\n\n";
     }
     _PROCESS_BASIC_INFORMATION64 pbi64 = readPibmemory(hprocess);
-
     peb = readPEBmemory(hprocess, pbi64);
     rtlUserProcParamsAddress = readRTLUserProcessParam(hprocess, pbi64.PebBaseAddress);
     commandLine = readRTLcommand(hprocess, rtlUserProcParamsAddress);
     commandLineContents = readRTLcommandLine(hprocess, commandLine);
+
     printf("Command Line : %.*S\n", commandLine.Length / 2, commandLineContents);
-    printf("\npib structure of the process PROCESS BASIC INFORMATION  x64 :\n");
-    printf("\n___________________________________________________________\n");
-    printf("pbi PEBBASEADDRESS : %p\n", pbi64.PebBaseAddress);
-    printf("pbi AFFINITYMASK : %u\n", pbi64.AffinityMask);
-    printf("pbi BASEPRIORITY : %d\n", pbi64.BasePriority);
-    printf("pbi EXITSTATUS : %p\n", pbi64.ExitStatus);
-    printf("pbi UNIQUEPROCESSID : %u\n", pbi64.uUniqueProcessId);
-    printf("pbi INHERITEDFROMUNIQUEPROCESSID : %u\n", pbi64.uInheritedFromUniqueProcessId);
-    printf("pbi RESERVED0 : %p\n", pbi64.Reserved0);
-    printf("pbi RESERVED1 : %p\n", pbi64.Reserved1);
-    printf("_____________________________________________________________\n");
-    printf("Process PEB structure \n");
+
+    std::cout << "\n___________________________________________________________\n";
+    std::cout << "\t\tPROCESS BASIC INFORMATION  x64 :\n";
+    std::cout << "pbi PEBBASEADDRESS :" << pbi64.PebBaseAddress << "\n";
+    std::cout << "pbi UNIQUEPROCESSID :" << pbi64.uUniqueProcessId << "\n";
+    std::cout << "_____________________________________________________________\n";
+    std::cout << "\t\tProcess PEB structure \n";
     if (peb.BeingDebugged == TRUE)
     {
-        printf("PEB Process debugged: ture\n");
+        std::cout << "PEB Process debugged: ture\n";
     }
     else
     {
-        printf("PEB Process debugged: false\n");
+        std::cout << "PEB Process debugged: false\n";
     }
-    printf("PEB AnsiCodePageData: %p\n", peb.AnsiCodePageData);
-    printf("PEB AtlThunkSListPtr32: %p\n", peb.AtlThunkSListPtr32);
-    if (peb.InheritedAddressSpace == TRUE)
-    {
-        printf("PEB InheritedAddressSpace: true\n");
-    }
-    else
-    {
-        printf("PEB InheritedAddressSpace: false\n");
-    }
-    printf("PEB CriticalSectionTimeout: %d\n", peb.CriticalSectionTimeout);
-    printf("PEB OSPlatformId: %p\n", peb.EnvironmentUpdateCount);
-    printf("PEB FastPebLock: %p\n", peb.FastPebLock);
-    printf("PEB FastPebLockRoutine: %p\n", peb.FastPebLockRoutine);
-    printf("PEB FastPebUnlockRoutine: %p\n", peb.FastPebUnlockRoutine);
-    printf("PEB FastPebUnlockRoutine: %p\n", peb.FreeList);
-    printf("PEB GdiDCAttributeList: %p\n", peb.GdiDCAttributeList);
-    printf("PEB GdiHandleBuffer: %p\n", peb.GdiHandleBuffer);
-    printf("PEB GdiSharedHandleTable: %p\n", peb.GdiSharedHandleTable);
-    printf("PEB HeapDeCommitFreeBlockThreshold: %p\n", peb.HeapDeCommitFreeBlockThreshold);
-    printf("PEB HeapDeCommitTotalFreeThreshold: %p\n", peb.HeapDeCommitTotalFreeThreshold);
-    printf("PEB HeapSegmentCommit: %p\n", peb.HeapSegmentCommit);
-    printf("PEB HeapSegmentReserve: %p\n", peb.HeapSegmentReserve);
-    printf("PEB ImageBaseAddress: %p\n", peb.ImageBaseAddress);
-    printf("PEB ImageProcessAffinityMask: %p\n", peb.ImageProcessAffinityMask);
-    printf("PEB ImageSubsystem: %p\n", peb.ImageSubsystem);
-    printf("PEB ImageSubsystemMajorVersion: %p\n", peb.ImageSubsystemMajorVersion);
-    printf("PEB ImageSubsystemMinorVersion: %p\n", peb.ImageSubsystemMinorVersion);
-
-    printf("PEB ProcessParameters: %p\n", peb.ProcessParameters);
-    printf("_____________________________________________________________\n");
+    std::cout << "_____________________________________________________________\n";
 
     return 0;
 }
